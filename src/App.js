e0ca256dd6d85c541f126a736598c5cd0a0e3ad1
Dimitar Atanasov <75276016+datanasovv@users.noreply.github.com>
2021-03-16 22:15:55 +0200

    added ColorBox and Palette components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import Palette from './Palette';
 import seedsColors from './seedColors';
+import { generatePalette } from './ColorHelpers';
 
 class App extends Component {
-  render(){
-    return (
-      <div>
-        <Palette {...seedsColors[4]}/>
-      </div>
-    );
+    render() {
+      console.log(generatePalette(seedsColors[4]));
+      return (
+        <div>
+          <Palette {...seedsColors[4]} />
+        </div>
+      );
+    }
   }
-}
 
 export default App;
--- src/seedColors.js
+++ src/seedColors.js
@@ -1,4 +1,4 @@
-export default [
+export default  [
   {
     paletteName: "Material UI Colors",
     id: "material-ui-colors",
