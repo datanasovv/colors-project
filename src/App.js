8cecf5728d7384e821f744b4f9d4a14d2d49f79b
Dimitar Atanasov <75276016+datanasovv@users.noreply.github.com>
2021-03-18 21:08:04 +0200

    Snackbar and Palette footer added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedsColors from './seedColors';
 import { generatePalette } from './ColorHelpers';
 
 class App extends Component {
-    render() {
-      return (
-        <div>
-          <Palette palette={generatePalette(seedsColors[4])} />
-        </div>
-      );
-    }
+  findPalette(id) {
+    return seedsColors.find(function (palette) {
+      return palette.id === id
+    })
   }
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>PALLETE LIST</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+      // <div>
+      //   <Palette palette={generatePalette(seedsColors[4])} />
+      // </div>
+    );
+  }
+}
 
 export default App;
